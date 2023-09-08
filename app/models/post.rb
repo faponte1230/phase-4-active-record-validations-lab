2783c7b6fe821f7e9376888679bd10a702eb4fc0
Fernando Aponte <faponte1230@yahoo.com>
2023-09-07 21:52:48 -0400

    commit tree 9cbbd5a74e4f5fc724644c046762c73b649d5959

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validate :clickbait_titles
+
+    
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+
+    def clickbait_titles
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title } 
+            errors.add(:title, "Title not allowed")
+        end
+    end
 end
